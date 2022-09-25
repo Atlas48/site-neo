542da0dba6a7b14254a348b08c3e67713d41223b
Atlas Cove <Atlas48@gmx.com>
2022-09-25 16:52:56 +0100

    Rendering via tape-and-string

--- src/render.sh
+++ src/render.sh
@@ -2,5 +2,6 @@
 declare -A title=([not_found]="404 - Page Not Found" [index]="Atlas48's Archives")
 for i in *.txti; do
   flatiron <$i> tmp.html
-  m4 -DTITLE=${title[${i##.html}]} template.m4.html > ../${i/txti/html}
+  m4 -DTITLE=${title[${i##.html}]} template.html.m4 > ../${i/txti/html}
+  rm tmp.html
 done
