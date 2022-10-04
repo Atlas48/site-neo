1d55329a210c724c849373ab71da21d13100f5bc
Atlas Cove <5618106+Atlas48@users.noreply.github.com>
2022-10-04 22:42:16 +0100

    general site work

--- src/render.sh
+++ src/render.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+# render.sh: part of the tape-and-string framework.
 declare -A title=([not_found]="404 - Page Not Found" [index]="Atlas48's Archives")
 for i in *.txti; do
-  flatiron <$i> tmp.html
-  m4 -DTITLE=${title[${i##.html}]} template.html.m4 > ../${i/txti/html}
-  rm tmp.html
+  flatiron < $i | m4 -DTITLE=${title[${i##.html}]} main.html.m4 > ../${i/txti/html}
 done
