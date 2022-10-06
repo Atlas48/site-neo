9130dd613e97ca4ee33c3fe9318a10a46dd47cf3
Atlas Cove <Atlas48@gmx.com>
2022-10-06 12:13:57 +0100

    prioritising removal of blank spaces

--- src/render.sh
+++ src/render.sh
@@ -2,5 +2,5 @@
 # render.sh: part of the tape-and-string framework.
 declare -A title=([not_found]="404 - Page Not Found" [index]="Atlas48's Archives")
 for i in *.txti; do
-  flatiron < $i | m4 -DTITLE=${title[${i##.html}]} main.html.m4 > ../${i/txti/html}
+  flatiron < $i | m4 -DTITLE=${title[${i##.html}]} main.html.m4 | sed '/^$/d' > ../${i/txti/html}
 done
