ada4fc972c1e9e0f301af6b0b44dcb8ac468a85e
Atlas Cove <Atlas48@gmx.com>
2023-01-21 23:23:04 +0000

    reorganising src files

--- src/_tape/render.sh
+++ src/_tape/render.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # render.sh: part of the tape-and-string framework.
+cd ..
 declare -A title=([not_found]="404 - Page Not Found" [index]="Atlas48's Archives")
 for i in *.txti; do
   flatiron < $i | m4 -DTITLE=${title[${i##.html}]} main.html.m4 | sed '/^$/d' > ../${i/txti/html}
