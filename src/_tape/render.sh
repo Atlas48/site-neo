412f929e3d05619c067c8c6fe304b67e91963c83
Atlas Cove <5618106+Atlas48@users.noreply.github.com>
2023-02-01 16:59:44 +0000

    before I test

--- src/_tape/render.sh
+++ src/_tape/render.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # render.sh: part of the tape-and-string framework.
 # v2.0
+source string.sh
 cd ..
-files=(`find -not -path '_*'`)
+files=(`find -not -path './_*'`)
 for i in ${files[@]}; do
   if test -d $i; then
 	if test -d ../$i; then
@@ -10,7 +11,7 @@ for i in ${files[@]}; do
 	else continue
 	fi
   elif test -f $i; then
-	tape $i | m4 -DTITLE=`${prog[title]}` ${prog[m4]} < $i ../$i
+	tape $i | m4 -DTITLE=`${prog[title]}` ${prog[m4]} < $i > ../$i
   else
 	echo "Skipping $i, unknown file type"
   fi
