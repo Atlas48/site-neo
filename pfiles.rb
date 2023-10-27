309f496642a79bef58436ce15dc7b3492afa7091
Atlas Cove <Atlas48@gmx.com>
2023-10-27 14:36:56 +0100

    reindent pfiles.rb

--- pfiles.rb
+++ pfiles.rb
@@ -4,53 +4,53 @@
 require 'find'
 ignore=!File.file?('dat/ignore.txt') ? [] : File.readlines('dat/ignore.txt')
 if ignore != []
-  ignore.map! do |i|
-    "in/#{i}"
-  end
+	ignore.map! do |i|
+		"in/#{i}"
+	end
 end
 class Enumerator
-  def collect
-    out=Array.new
-    for i in self
-      out.push(i)
-    end
-    return out
-  end
+	def collect
+		out=Array.new
+		for i in self
+			out.push(i)
+		end
+		return out
+	end
 end
 list=Find.find('in')
 l=list.collect
 case ARGV.first
 when "doc"
-  for i in l do
-    next if ignore.include?(i) or /\.v.html/.match?(i)
-    if /\.(txti|org|md|html)$/.match?(i)
-      print i
-      print ' ' unless i==l.last
-    end
-  end
+	for i in l do
+		next if ignore.include?(i) or /\.v.html/.match?(i)
+		if /\.(txti|org|md|html)$/.match?(i)
+			print i
+			print ' ' unless i==l.last
+		end
+	end
 when "sass"
-  for i in l do
-    next if ignore.include?(i)
-     if /\.s[ac]ss$/.match?(i)
-      print i
-      print ' ' unless i==l.last
-     end
-  end
+	for i in l do
+		next if ignore.include?(i)
+		if /\.s[ac]ss$/.match?(i)
+			print i
+			print ' ' unless i==l.last
+		end
+	end
 when "dir"
-  for i in l do
-    next if ignore.include?(i)
-    if File.directory?(i)
-      print i
-      print ' ' unless i==l.last
-    end
-  end
+	for i in l do
+		next if ignore.include?(i)
+		if File.directory?(i)
+			print i
+			print ' ' unless i==l.last
+		end
+	end
 when "rest"
-  for i in l do
-    next if ignore.include?(i)
-    unless /\.(s[ac]ss|txti|org|md)$/.match?(i) or File.directory(i)
-      print i
-      print ' ' unless i==l.last
-    end
-  end
+	for i in l do
+		next if ignore.include?(i)
+		unless /\.(s[ac]ss|txti|org|md)$/.match?(i) or File.directory(i)
+			print i
+			print ' ' unless i==l.last
+		end
+	end
 else
 end
